1580ea6bee9881dbf1caeb7692c2bc5062a3337a
Alejandro <77583757+betanS@users.noreply.github.com>
2025-08-21 00:25:32 +0100

    Agregar archivos de proyecto.

---
+++ PokedexApi/Controllers/PokemonController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PokedexApi.Models;
+
+namespace PokedexApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PokemonController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public PokemonController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("GetPokemons")]
+        public async Task<IActionResult> GetPokemons()
+        {
+            var result = await _context.Pokemon.Select(x => new Pokemon
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Type = x.Type,
+                Weight = x.Weight,
+                Height = x.Height
+            }).ToListAsync();
+
+            return Ok(result);
+        }
+
+        [HttpGet("CreatePokemon")]
+        public async Task<IActionResult> CreatePokemon([FromBody] Pokemon pokemon)
+        {
+            _context.Pokemon.Add(pokemon);
+            await _context.SaveChangesAsync();
+
+            return Ok(pokemon);
+        }
+
+        [HttpGet("EditPokemon")]
+        public async Task<IActionResult> EditPokemon([FromBody] Pokemon pokemon)
+        {
+            var rows = await _context.Pokemon.Where(x => x.Id == pokemon.Id)
+                .ExecuteUpdateAsync(x => x.SetProperty(x => x.Name, pokemon.Name));
+
+            return Ok(pokemon);
+        }
+
+        [HttpGet("DeletePokemon")]
+        public async Task<IActionResult> DeletePokemon(int id)
+        {
+            var rows = await _context.Pokemon.Where(x => x.Id == id).ExecuteDeleteAsync();
+
+            return Ok(true);
+        }
+    }
+}
---
+++ PokedexApi/Models/Pokemon.cs
@@ -0,0 +1,11 @@
+﻿namespace PokedexApi.Models
+{
+    public class Pokemon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Weight { get; set; }
+        public int Height { get; set; }
+    }
+}
